6cd4127fc5da3a302e877dee8dbdf3d45804b820
Ashish Devkar <ashishdevkar761@gmail.com>
2026-07-30 19:50:05 +0530

    make changes in cors for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,10 @@ require("dotenv").config()
 const app = express()
 
 // Middleware
-app.use(cors())
+app.use(cors({
+    origin: 'https://local-service-finder-two.vercel.app/',
+    credentials: true
+}));
 app.use(express.json())
 
 // Routes
@@ -40,13 +43,16 @@ app.get("/", (req, res) => {
 })
 
 // Connect to MongoDB
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        console.log("MongoDB Connected")
-        app.listen(process.env.PORT, () => {
-            console.log(`Server running on port ${process.env.PORT}`)
-        })
+        console.log("MongoDB Connected");
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
     })
     .catch((err) => {
-        console.log("MongoDB Connection error:", err)
-    })
+        console.log("MongoDB Connection error:", err);
+    });
